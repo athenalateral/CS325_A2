7bc73e6b012aa41de37a42f0aeb5fc3e28ab5db9
athenalateral <gumplesteen21@gmail.com>
2024-06-10 17:13:39 -0500

    Abstract_Base_Class.py

--- test.py
+++ test.py
@@ -1,3 +1,21 @@
-print ("Eureka!")
+# Abstract base classes
+from abc import ABC, abstractmethod
 
-print ("there is another")
+class Employee(ABC):
+    @abstractmethod
+    def arrive_at_work(self):
+        pass
+
+class Manager(Employee):
+    def arrive_at_work(self):
+        print("Arriving at job headquarters!")
+
+class Supervisor(Employee):
+    def arrive_at_work(self):
+        print("Reporting for duty at job job!")
+
+Isaac = Manager()
+Clark = Supervisor()
+
+Isaac.arrive_at_work()
+Clark.arrive_at_work()
